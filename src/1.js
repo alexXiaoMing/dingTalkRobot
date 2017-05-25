4c92fd1d801486b28e8a7865685b97876600ab52
alexxiaoming <nounou8675@gmail.com>
2017-05-25 16:30:35 +0800

    Initial commit

---
+++ src/1.js
@@ -0,0 +1,20 @@
+/**
+ * Created by hayden on 2017/5/25.
+ */
+var request = require('request');
+var  options = {
+    method: 'post',
+    url: u,
+    form: content,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+};
+
+request(options, function (err, res, body) {
+    if (err) {
+        console.log(err)
+    }else {
+        console.log(body);
+    }
+})
